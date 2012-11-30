3afef1b101a83f2b78ab244405b1b4e46ddefc6f
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2012-11-30 22:25:55 +0400

    ORM-only Pony version

--- pony/tests/orm/coverage.bat
+++
@@ -1,3 +0,0 @@
-C:\Python27\Scripts\coverage.exe erase 
-C:\Python27\Scripts\coverage.exe run --source pony.dbapiprovider,pony.dbschema,pony.decompiling,pony.orm,pony.sqlbuilding,pony.sqltranslation --branch test_all.py
-C:\Python27\Scripts\coverage.exe html
